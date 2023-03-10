bce2fcf5412d1e4e7a854ef8d35708e41081e8bf
pausaroz <paulka_rozek@wp.pl>
2023-03-10 16:19:48 +0100

    Stringi - podstawy

---
+++ chapt_4/string.py
@@ -0,0 +1,14 @@
+type("Hello") # Can use this in interactive window (IDLE)
+string3 = "We're #1!"
+string4 = 'I said. "Put it over by the llama."'
+# text = "She said, "What time is it?"" # SyntaxError: invalid syntax
+# zakomentuj linie kodu = alt + 3
+# odkomentuj linie kodu = alt + 4
+
+text = "She said, \"What time is it?\""
+print(text)  # She said, "What time is it?"
+
+len(text) # Determine the Length of a String.
+# Multiline Strings
+# The PEP 8 style guide recommends that each line of Python code
+# contain no more than seventy-nine characters—including spaces. 
