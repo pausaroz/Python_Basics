8274a9f23ffafe57098d55b18891b63d705cee9d
pausaroz <paulka_rozek@wp.pl>
2023-03-13 17:11:28 +0100

    Stringi - chapter 4.1

--- chapt_4/string.py
+++ chapt_4/string.py
@@ -11,4 +11,31 @@ print(text)  # She said, "What time is it?"
 len(text) # Determine the Length of a String.
 # Multiline Strings
 # The PEP 8 style guide recommends that each line of Python code
-# contain no more than seventy-nine characters—including spaces. 
+# contain no more than seventy-nine characters—including spaces.
+
+paragraph_using_blackslash_method = "This planet has—or rather had—a problem, which was \
+this: most of the people living on it were unhappy for pretty much \
+of the time. Many solutions were suggested for this problem, but \
+most of these were largely concerned with the movements of small \
+green pieces of paper, which is odd because on the whole it wasn't \
+the small green pieces of paper that were unhappy."
+
+paragraph_using_triple_quoted = """This planet has—or rather had—a problem, which was
+this: most of the people living on it were unhappy for pretty much
+of the time. Many solutions were suggested for this problem, but
+most of these were largely concerned with the movements of small
+green pieces of paper, which is odd because on the whole it wasn't
+the small green pieces of paper that were unhappy."""
+
+print(" ")
+print("Using triple quoted")
+print("""An example of a ...
+string that spans across multiple lines ...
+and also preserves whitespace.""")
+
+print(" ")
+print("Using blackslash method")
+print("Przykład ... \
+string z  ... \
+backslash.")
+
