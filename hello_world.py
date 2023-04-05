a3db141bf2a6291e5b2f203b74d30157161c0d24
pausaroz <paulka_rozek@wp.pl>
2023-04-05 23:39:54 +0200

    create and edit file: hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+# ------3.2------
+print("Hello world!")
+
+# ------3.3------
+greeting = "Hello, World"
+print(greeting)
+
+# error:
+# print(Greeting)
+
+# bad name var
+s = 3600
+
+# better name
+second = 3600
+
+# the best name
+second_per_hour = 3600
+
+# ------3.4------
+x = 2
+print(x)
+y = "2"
+print(y)
+
+# ------3.5------
+# This is a block comment
+print(greeting)  # This is a inline comment
+print("#1")
