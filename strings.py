5cebd4b5d83ad5f0b08a55d94b9c1940b957fd6d
pausaroz <paulka_rozek@wp.pl>
2023-04-05 23:55:35 +0200

    create and edit file: strings.py

---
+++ strings.py
@@ -0,0 +1,22 @@
+# ------4.1------
+print(type("Hello, world"))
+pharse = "Hello, world"
+print(type(pharse))
+
+string1 = 'Hello, world'
+string2 = "1234"
+string3 = "We're #1!"
+string4 = 'I said, "Put it over by the llama."'
+
+# error
+# text = "She said, "What time is it?""
+
+text = "She said, \"What time is it?\""
+print(text)
+print(len("abc"))
+letters = "abc"
+print(len(letters))
+
+
+
+
