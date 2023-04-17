5dde9c693b956045457c68359201ba0d8b1bd708
pausaroz <paulka_rozek@wp.pl>
2023-04-17 20:13:26 +0200

    edit file function_and_loops.py

--- functions_and_loops.py
+++ functions_and_loops.py
@@ -19,10 +19,55 @@ type(return_value)  # <class 'NoneType'>
 print(return_value)  # None
 
 # ------6.2------
+
 def multiply(x, y):  # Function signature
     # F. body
     product = x * y
     return product
+    # any code appears below the return will never run
+    print("You cant't see me!")
 
 num = multiply(1, 3)
 print(num)  # 3
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+greet("Dave")  # Hello, Dave!
+return_value = greet("Dave")  # Hello, Dave!
+print(return_value)  # None
+
+help(len)
+# Help on built-in function len in module builtins:
+#
+# len(obj, /)
+#     Return the number of items in a container.
+
+
+help(multiply)
+# Help on function multiply in module __main__:
+# 
+# multiply(x, y)
+
+
+# docstring
+
+def multiply(x, y): 
+    """Return the product of two numbers x and y."""
+    product = x * y
+    return product
+    
+######
+
+def cube(number):
+    """Return the cube of the input number."""
+    cube_num = num**3  # pow(num, 3)
+    return cube_num
+
+print(cube(3))
+
+######
+
+# ------6.3------
+
