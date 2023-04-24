24e20f0fed880da006230da4abd9ddbc4cd3713b
pausaroz <paulka_rozek@wp.pl>
2023-04-24 21:42:45 +0200

    rename files

--- chapt_5/exponent.py
+++
@@ -1,3 +0,0 @@
-number_1 = float(input("Enter a base: "))
-number_2 = float(input("Enter an exponent: "))
-print(f"{number_1} to the power of {number_2} = {number_1 ** number_2}")
--- number_exponent.py
+++
@@ -1,4 +0,0 @@
-# ------5.3------
-number_1 = float(input("Enter a base: "))
-number_2 = float(input("Enter an exponent: "))
-print(f"{number_1} to the power of {number_2} = {number_1 ** number_2}")
