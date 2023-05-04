bb6b4591bfafde29cce84a0a7ef00157876ec613
pausaroz <paulka_rozek@wp.pl>
2023-05-04 14:00:27 +0200

    edit file

--- chapt_9_tuples_lists_dictionaries.py
+++ chapt_9_tuples_lists_dictionaries.py
@@ -86,3 +86,141 @@ my_name = tuple("Paulina")
 #6.
 my_name[1:] #  ('a', 'u', 'l', 'i', 'n', 'a')
 ######
+
+# ------9.2------
+
+
+colors = ["red", "yellow", "green", "blue"]
+colors #  ['red', 'yellow', 'green', 'blue']
+type(colors) #  <class 'list'>
+colors #  ['red', 'yellow', 'green', 'blue']
+
+list((1, 2, 3)) #  [1, 2, 3]
+list("Python") #  ['P', 'y', 't', 'h', 'o', 'n']
+
+groceries = "eggs, milk, cheese"
+grocery_list = groceries.split(", ")
+grocery_list #  ['eggs', 'milk', 'cheese']
+
+"a;b;c".split(";") #  ['a', 'b', 'c']
+"The quick brown fox".split(" ") #  ['The', 'quick', 'brown', 'fox']
+"abbaabba".split("ba") #  ['ab', 'ab', '']
+"abbaabba".split("c") #  ['abbaabba']
+
+
+numbers = [1, 2, 3, 4]
+numbers[1] #  2
+numbers[1:3] #  [2, 3]
+
+"Bob" in numbers #  False
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+        
+#2
+#4
+        
+colors #  ['red', 'yellow', 'green', 'blue']
+colors[0] = "burgundy"
+colors #  ['burgundy', 'yellow', 'green', 'blue']
+
+colors = ["red", "yellow", "green", "blue"]
+colors[1:3] = ["orange", "megenta", "aqua"]
+colors #  ['red', 'orange', 'megenta', 'aqua', 'blue']
+
+colors #  ['red', 'orange', 'megenta', 'aqua', 'blue']
+colors[1:4] = ["yellow", "green"]
+colors #  ['red', 'yellow', 'green', 'blue']
+
+#list.insert()
+colors #  ['red', 'yellow', 'green', 'blue']
+colors.insert(1, "orange")
+colors #  ['red', 'orange', 'yellow', 'green', 'blue']
+
+colors.insert(10, "violet")
+colors #  ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+
+colors #  ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+colors.insert(-1, "indigo")
+colors #  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+#list.pop()
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+color = colors.pop(3)
+color #  'green'
+colors #  ['red', 'orange', 'yellow', 'blue', 'indigo', 'violet']
+
+#colors.pop(10) #  IndexError: 
+colors.pop(-1) #  'violet'
+colors #  ['red', 'orange', 'yellow', 'blue', 'indigo']
+colors.pop() #  'indigo'
+colors #  ['red', 'orange', 'yellow', 'blue']
+
+#list.append()
+colors.append("indygo")
+colors # ['red', 'orange', 'yellow', 'blue', 'indygo']
+
+colors.pop() #  'indygo'
+colors # ['red', 'orange', 'yellow', 'blue']
+
+colors.insert(len(colors), "indygo")
+colors #  ['red', 'orange', 'yellow', 'blue', 'indygo']
+
+colors #  ['red', 'orange', 'yellow', 'blue', 'indygo']
+colors.extend(["violet", "ultraviolet"])
+colors #  ['red', 'orange', 'yellow', 'blue', 'indygo', 'violet', 'ultraviolet']
+
+colors.pop() #  'ultraviolet'
+colors.pop() #  'violet'
+colors #  ['red', 'orange', 'yellow', 'blue', 'indygo']
+colors.extend(("violet", "ultraviolet"))
+colors #  ['red', 'orange', 'yellow', 'blue', 'indygo', 'violet', 'ultraviolet']
+
+nums =[1, 2, 3, 4, 5]
+total = 0
+for number in nums:
+    total = total + number
+    
+total #  15
+sum([1, 2, 3, 4 ,5]) #  15
+#sum([1, 2, 3, "four" ,5]) #  TypeError
+min([1, 2, 3, 4 ,5]) #  1
+max([1, 2, 3, 4 ,5]) #  5
+
+
+numbers = (1, 2, 3, 4, 5)
+squares = [num**2 for num in numbers]
+squares #  [1, 4, 9, 16, 25]
+
+squares = []
+for num in numbers:
+    squares.append(num**2)
+
+squares #  [1, 4, 9, 16, 25]
+
+str_numbers = ["1.5", "2.3", "5.25"]
+float_numbers = [float(value) for value in str_numbers]
+float_numbers #  [1.5, 2.3, 5.25]
+
+######
+#1.
+food = ["rice", "beans"]
+#2.
+food.append("broccoli")
+food #  ['rice', 'beans', 'broccoli']
+#3.
+food.extend(["bread", "pizza"])
+food #  ['rice', 'beans', 'broccoli', 'bread', 'pizza']
+#4.
+print(food[:2]) #  ['rice', 'beans']
+#5.
+print(food[len(food)-1]) #  pizza
+print(food[-1]) #  pizza
+#6.
+breakfast = "eggs, fruit, orange    juice".split(", ")
+#7.
+len(breakfast) == 3
+#8.
+lengths = [len(eat) for eat in breakfast]
+######
