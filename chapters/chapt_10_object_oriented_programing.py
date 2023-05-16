2e9cbd09c6be24705bc9698e92c6392648ef6881
pausaroz <paulka_rozek@wp.pl>
2023-05-16 21:12:57 +0200

    add and edit file: chapters/chapt_10_object_oriented_programing.py

---
+++ chapters/chapt_10_object_oriented_programing.py
@@ -0,0 +1,16 @@
+# ------10.1------
+
+kirk = ["James Kirk", 34, "Captain", 2265]
+spock = ["Spock", 35, "Science Officer", 2254]
+mccoy = ["Leonard McMoy", "Chief Medical Offcer", 2266]
+
+class Dog:
+    
+    # Class attribute
+    species = "Canis familiaris"
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age= age
+
+# ------10.2------
